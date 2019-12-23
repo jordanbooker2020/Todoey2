f085394662b31d3ce321045b5e406ca1dcad5639
Jordan Booker <jordanlbooker2020@gmail.com>
2019-12-22 22:01:17 -0500

    Data Persi

--- Todoey2/AppDelegate.swift
+++ Todoey2/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //Shows where the plist file is in the finder
+       /* print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)*/
+        
         return true
     }
 
