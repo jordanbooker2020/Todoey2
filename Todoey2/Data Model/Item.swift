956cdadd91ef04f76e52d6cb0514a2c407c9759d
Jordan Booker <jordanlbooker2020@gmail.com>
2019-12-23 20:26:47 -0500

    Convert to MVC design pattern 2.

---
+++ Todoey2/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey2
+//
+//  Created by jbooker2016 on 12/23/19.
+//  Copyright © 2019 jbooker2016. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
